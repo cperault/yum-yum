0bcdaef6b7552b9f84d31f5a30ab54a220f603dc
Christopher Perault <cperault92@gmai.com>
2021-12-18 09:45:00 -0700

    Fixed layout constraint that was causing overlap of labels. Added rounded corners to image.

--- Yum Yum/Utility/RemoteImageUtility.swift
+++ Yum Yum/Utility/RemoteImageUtility.swift
@@ -9,10 +9,10 @@ import UIKit
 
 func getRemoteImage(mealImageURL: String) -> UIImage? {
     guard let imageURL: URL = URL(string: mealImageURL) else {
-        return UIImage(named: "kitchen")
+        return UIImage(named: "kitchen")?.roundedImage
     }
     guard let imageData: Data = try? Data(contentsOf: imageURL) else {
-        return UIImage(named: "kitchen")
+        return UIImage(named: "kitchen")?.roundedImage
     }
-    return UIImage(data: imageData)
+    return UIImage(data: imageData)?.roundedImage
 }
