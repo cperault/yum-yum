b406f2ba8f5d14eceaf6f109056342ed4506d9b8
Christopher Perault <cperault92@gmai.com>
2021-12-21 13:21:18 -0700

    Updated color of nav bar and tab bar item text

--- Yum Yum/App/AppDelegate.swift
+++ Yum Yum/App/AppDelegate.swift
@@ -11,6 +11,8 @@ import CoreData
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let navigationBarAppearance = UINavigationBar.appearance()
+        navigationBarAppearance.tintColor = UIColor.black
         return true
     }
     
